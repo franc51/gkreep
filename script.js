a41401692cf8af7940286c04934c100b1612c524
franc51 <101558212+franc51@users.noreply.github.com>
2023-04-04 23:49:46 +0300

    changed design

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-let input = document.getElementById("save-button");
+const popup = document.querySelector(".add-btn");
+const popupBox = document.querySelector(".popup-box");
+const overlay = document.querySelector(".overlay");
+const exitOverlay = document.querySelector(".exit-popup");
+popup.addEventListener("click", () => {
+  popupBox.classList.add("show-popup");
+  overlay.classList.add("show-overlay");
+  console.log(popupBox);
+  console.log(overlay);
+});
 
-input.addEventListener("click", createNote);
-
-function createNote() {
-  let inputText = document.getElementById("description").value;
-
-  let newDiv = document.createElement("div");
-  let newH1 = document.createElement("h2");
-  let newbtn = document.createElement("button");
-  newbtn.innerHTML = "Delete note";
-  newH1.innerHTML = inputText;
-
-  newH1.classList.add("note-style");
-  newDiv.classList.add("card");
-  newbtn.classList.add("delete-button");
-  newbtn.setAttribute("id", "delete-button");
-
-  let container = document.getElementById("note-list-container");
-
-  container.appendChild(newDiv);
-  newDiv.append(newH1);
-  newDiv.append(newbtn);
-
-  inputText = "";
-}
+exitOverlay.addEventListener("click", () => {
+  popupBox.classList.remove("show-popup");
+  overlay.classList.remove("show-overlay");
+  console.log(popupBox);
+  console.log(overlay);
+});
