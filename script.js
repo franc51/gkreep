31fe27a749c38743116ded1960dd39a7420af359
franc51 <101558212+franc51@users.noreply.github.com>
2023-04-15 21:22:37 +0300

    changed design

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
+let notes = [];
+
 const popup = document.querySelector(".add-btn");
 const popupBox = document.querySelector(".popup-box");
 const overlay = document.querySelector(".overlay");
 const exitOverlay = document.querySelector(".exit-popup");
+const deleteNote = document.querySelector(".remove-btn");
+
+let titleValue = document.getElementById("title");
+let descriptionValue = document.getElementById("description");
+const addButton = document.querySelector(".add-note-btn");
+
+titleValue.addEventListener("change", (e) => {
+titleValue.value = e.target.value;
+})
+descriptionValue.addEventListener("change", (e) => {
+  descriptionValue.value = e.target.value;
+  })
 popup.addEventListener("click", () => {
   popupBox.classList.add("show-popup");
   overlay.classList.add("show-overlay");
-  console.log(popupBox);
-  console.log(overlay);
 });
 
 exitOverlay.addEventListener("click", () => {
   popupBox.classList.remove("show-popup");
   overlay.classList.remove("show-overlay");
-  console.log(popupBox);
-  console.log(overlay);
 });
+
+addButton.addEventListener("click", () => {
+  let note = new Note(titleValue.value, descriptionValue.value);
+  notes.push(note);
+  console.log(notes);
+  refresh();
+});
+
+class Note {
+  constructor(title, description){
+    this.title = title;
+    this.description = description;
+    this.id = 1;
+  }
+}
+
+function refresh(){
+  notes.map((note) => {
+    let infoContainer = document.querySelector(".info");
+    let Title = document.createElement("h1");
+    let TitleText = document.createTextNode(note.title);
+    Title.appendChild(TitleText);
+    infoContainer.appendChild(Title);
+  } )
+}
